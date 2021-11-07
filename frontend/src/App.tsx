e472d2379ad856a769f054c320971f7fe3589930
AndersonPorphirioFernandes <andersonporphiriofernandes@gmail.com>
2021-11-06 23:25:02 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import Datatable from "components/Datatable";
+
 function App() {
   return (
-    <div className="App">
+    <>
+    <NavBar />
+    <div className="container">
     <h1 className="text-primary">Ola mundo!</h1>
+    <Datatable />
     </div>
+
+    <Footer />
+    </>
   );
 }
 
